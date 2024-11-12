c0ba7e9d4d42ea67e7fe9084f4e88a99c3edf604
Won, Kee-hoon <lasnier@naver.com>
2024-11-12 20:57:25 +0900

    2411122057

--- Baekjoon/17472.다리_만들기2/17472.다리_만들기2.py
+++ Baekjoon/17472.다리_만들기2/17472.다리_만들기2.py
@@ -1,52 +1,91 @@
 import sys
 from collections import deque
+from heapq import heappop, heappush
 sys.stdin = open('input.txt')
 input = sys.stdin.readline
 
 # 델타 배열
-dr = [0, 1]
-dc = [1, 0]
+dr = [0, 1, 0, -1]
+dc = [1, 0, -1, 0]
 
 
-def find_island(map, is_visited=None):
-    if is_visited is None:
-        is_visited = [[0] * M for _ in range(N)]
+def find_island(map):
+    is_visited = [[0] * M for _ in range(N)]
 
     for r in range(N):
         for c in range(M):
             if not is_visited[r][c]:
                 is_visited[r][c] = 1
                 if map[r][c]:
-                    head = (N - 1, M - 1)
-                    tail = (0, 0)
+                    borders = []
                     order = deque([(r, c)])
                     while order:
                         r, c = order.popleft()
-                        head = min(head, (r, c))
-                        tail = max(tail, (r, c))
-                        for dir in range(2):
+                        is_border = False
+                        for dir in range(4):
                             nr = r + dr[dir]
                             nc = c + dc[dir]
-                            if 0 <= nr < N and 0 <= nc < M and map[nr][nc]:
-                                is_visited[nr][nc] = 1
-                                order.append((nr, nc))
-                    islands.append((head, tail))
+                            if 0 <= nr < N and 0 <= nc < M:
+                                if map[nr][nc]:
+                                    if not is_visited[nr][nc]:
+                                        is_visited[nr][nc] = 1
+                                        order.append((nr, nc))
+                                elif not map[nr][nc]:
+                                    is_border = True
+                        if is_border:
+                            borders.append((r, c))
+                    islands.append(borders)
 
 
 def find_edge(nodes):
-    for start in len(nodes):
-        for end in len(nodes):
-            if nodes[start] == nodes[end]:
+    N = len(nodes)
+    for start in range(N):
+        for end in range(N):
+            if start == end:
                 continue
-            if nodes[start][1][0] >= nodes[end][0][0] or nodes[start][0][0] <= nodes[end][1][0]:
-                distance = min(abs(nodes[start][1][1] - nodes[end][0][1]), abs(nodes[start][0][1] - nodes[end][1][1]))
-                if distance >= 2:
-                    adjacency_list[start].append((end, distance))
-            elif
+            distance = 10
+            tmp_dis = 0
+            for border1 in nodes[start]:
+                for border2 in nodes[end]:
+                    if border1[0] == border2[0]:
+                        tmp_dis = abs(border1[1] - border2[1]) - 1
+                    elif border1[1] == border2[1]:
+                        tmp_dis = abs(border1[0] - border2[0]) - 1
+                    if 0 < tmp_dis:
+                        distance = min(distance, tmp_dis)
+            if 2 <= distance < 10:
+                adjacency_list[start].append((distance, end))
+
+
+def prim(adj):
+    N = len(adj)
+    is_visited = [0] * N
+    order = [(0, 0)]
+    total_cost = 0
+
+    while order:
+        weight, node = heappop(order)
+        if is_visited[node]:
+            continue
+        is_visited[node] = 1
+        total_cost += weight
+
+        for w_v in adj[node]:
+            if not is_visited[w_v[1]]:
+                heappush(order, w_v)
+
+    if sum(is_visited) < N:
+        return -1
+
+    return total_cost
+
 
 N, M = map(int, input().split())
 beach_map = [list(map(int, input().split())) for _ in range(N)]
 islands = []
 find_island(beach_map)
-adjacency_list = [[] for _ in range(len(islands))]
 print(islands)
+adjacency_list = [[] for _ in range(len(islands))]
+find_edge(islands)
+print(adjacency_list)
+print(prim(adjacency_list))
---
+++ Baekjoon/17472.다리_만들기2/17472.다리_만들기22.py
@@ -0,0 +1,119 @@
+import sys
+from collections import deque
+from heapq import heappop, heappush
+sys.stdin = open('input.txt')
+input = sys.stdin.readline
+
+# 델타 배열
+dr = [0, 1, 0, -1]
+dc = [1, 0, -1, 0]
+
+
+def find_island(map):
+    is_visited = [[0] * M for _ in range(N)]
+
+    for r in range(N):
+        for c in range(M):
+            if not is_visited[r][c]:
+                is_visited[r][c] = 1
+                if map[r][c]:
+                    borders = set()
+                    order = deque([(r, c)])
+                    while order:
+                        r, c = order.popleft()
+                        is_border = False
+                        for dir in range(4):
+                            nr = r + dr[dir]
+                            nc = c + dc[dir]
+                            if 0 <= nr < N and 0 <= nc < M:
+                                if map[nr][nc]:
+                                    if not is_visited[nr][nc]:
+                                        is_visited[nr][nc] = 1
+                                        order.append((nr, nc))
+                        borders.add((r, c))
+                    islands.append(borders)
+
+
+def find_edge(nodes):
+    n = len(nodes)
+    for start in range(n):
+        for point in nodes[start]:
+            to_right = point[1] + 1
+            distance = (10, False)
+            is_finish = False
+            while to_right < M:
+                if (point[0], to_right) in nodes[start]:
+                    break
+                if beach_map[point[0]][to_right]:
+                    tmp_dis = abs(point[1] - to_right) - 1
+                    if 2 <= tmp_dis < distance[0]:
+                        for idx in range(n):
+                            if (point[0], to_right) in nodes[idx]:
+                                distance = (tmp_dis, idx)
+                                is_finish = True
+                                break
+                    else:
+                        is_finish = True
+                else:
+                    to_right += 1
+                if is_finish:
+                    break
+            if distance[1] is not False:
+                adjacency_list[start].append(distance)
+                adjacency_list[distance[1]].append((distance[0], start))
+
+            to_below = point[0] + 1
+            distance = (10, False)
+            is_finish = False
+            while to_below < N:
+                if (to_below, point[1]) in nodes[start]:
+                    break
+                if beach_map[to_below][point[1]]:
+                    tmp_dis = abs(point[0] - to_below) - 1
+                    if 2 <= tmp_dis < distance[0]:
+                        for idx in range(n):
+                            if (to_below, point[1]) in nodes[idx]:
+                                distance = (tmp_dis, idx)
+                                is_finish = True
+                                break
+                    else:
+                        is_finish = True
+                else:
+                    to_below += 1
+                if is_finish:
+                    break
+            if distance[1] is not False:
+                adjacency_list[start].append(distance)
+                adjacency_list[distance[1]].append((distance[0], start))
+
+
+def prim(adj):
+    N = len(adj)
+    is_visited = [0] * N
+    order = [(0, 0)]
+    total_cost = 0
+
+    while order:
+        weight, node = heappop(order)
+        if is_visited[node]:
+            continue
+        is_visited[node] = 1
+        total_cost += weight
+
+        for w_v in adj[node]:
+            if not is_visited[w_v[1]]:
+                heappush(order, w_v)
+
+    if sum(is_visited) < N:
+        return -1
+
+    return total_cost
+
+
+N, M = map(int, input().split())
+beach_map = [list(map(int, input().split())) for _ in range(N)]
+islands = []
+find_island(beach_map)
+adjacency_list = [[] for _ in range(len(islands))]
+find_edge(islands)
+print(prim(adjacency_list))
