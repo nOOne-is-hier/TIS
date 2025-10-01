9b93bbe3d43fa02cc3f6bc52afff2c2302750b63
Won, Kee-hoon <amorinael@gmail.com>
2025-10-01 23:26:24 +0900

    2510012326

---
+++ Baekjoon/30403.무지개_만들기/30403.무지개_만들기.cpp
@@ -0,0 +1,95 @@
+// 30403 – 무지개 만들기
+// https://www.acmicpc.net/problem/30403
+// solved.ac: https://solved.ac/search?query=30403
+// 시간 제한: 1 초
+// 메모리 제한: 1024 MB
+// 티어: 🟫 Bronze II
+// 태그: 구현, 문자열
+// 푼 사람 수: 666
+// 평균 시도: 1.70
+
+#include <iostream>
+#if defined(_WIN32)
+#include <io.h>
+#include <cstdio>
+#else
+#include <unistd.h>
+#include <cstdio>
+#endif
+
+using namespace std;
+
+int main()
+{
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr);
+
+  // 표준입력이 터미널이면 input.txt로 대체 (파이프 입력 시에는 그대로 cin)
+#if defined(_WIN32)
+  if (_isatty(_fileno(stdin)))
+  {
+    freopen("input.txt", "r", stdin);
+  }
+#else
+  if (isatty(fileno(stdin)))
+  {
+    freopen("input.txt", "r", stdin);
+  }
+#endif
+
+  int N;
+  string s;
+  cin >> N >> s;
+
+  string upstring = "ROYGBIV";
+  string lostring = "roygbiv";
+  bool up = true, lo = true;
+
+  for (char &us : upstring)
+  {
+    bool found = false;
+    for (char &c : s)
+      if (us == c)
+      {
+        found = true;
+        break;
+      }
+
+    if (!found)
+    {
+      up = false;
+      break;
+    }
+  }
+
+  for (char &ls : lostring)
+  {
+    bool found = false;
+    for (char &c : s)
+      if (ls == c)
+      {
+        found = true;
+        break;
+      }
+
+    if (!found)
+    {
+      lo = false;
+      break;
+    }
+  }
+
+  if (up && lo)
+    cout << "YeS";
+
+  else if (up)
+    cout << "YES";
+
+  else if (lo)
+    cout << "yes";
+
+  else
+    cout << "NO!";
+
+  return 0;
+}
