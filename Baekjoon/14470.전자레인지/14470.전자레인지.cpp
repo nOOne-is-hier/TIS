35ae496debb78cf137742e8b027ebb52fff6f2e2
WON, KEEHOON <lasnier@naver.com>
2025-11-19 08:37:43 +0900

    2511190837

---
+++ Baekjoon/14470.전자레인지/14470.전자레인지.cpp
@@ -0,0 +1,44 @@
+// 14470 – 전자레인지
+// https://www.acmicpc.net/problem/14470
+// solved.ac: https://solved.ac/search?query=14470
+// 시간 제한: 2 초
+// 메모리 제한: 512 MB
+// 티어: 🟫 Bronze IV
+// 태그: 구현, 사칙연산, 수학, 시뮬레이션
+// 푼 사람 수: 5,300
+// 평균 시도: 1.30
+
+#include <iostream>
+#if defined(_WIN32)
+#include <io.h>
+#include <cstdio>
+#else
+#include <unistd.h>
+#include <cstdio>
+#endif
+
+using namespace std;
+
+int main()
+{
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr);
+
+  // 표준입력이 터미널이면 input.txt로 대체 (파이프 입력 시에는 그대로 cin)
+#if defined(_WIN32)
+  if (_isatty(_fileno(stdin)))
+  {
+    freopen("input.txt", "r", stdin);
+  }
+#else
+  if (isatty(fileno(stdin)))
+  {
+    freopen("input.txt", "r", stdin);
+  }
+#endif
+
+  int A, B, C, D, E;
+  cin >> A >> B >> C >> D >> E;
+  cout << (A > 0 ? (B - A) * E : -A * C + D + B * E);
+  return 0;
+}
